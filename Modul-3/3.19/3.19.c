18e8bc71cc5f938de82cf3112fa7ee787c7f5dd3
Svetlana Rudez <wls@LAPTOP-FNK17HHU.localdomain>
2021-05-15 21:54:30 +0200

    Laget fil med koden til oppgaven 3.19 i Modul-3

---
+++ Modul-3/3.19/3.19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+       pid_t pid = fork(); // fork a child process
+
+    if (pid > 0) //if pid greater than 0 - parent process
+{
+       sleep(20); //to create zombie process we can't use wait(), but we can use sleep() systemcall
+       printf("This is the parent process\n");
+
+    }
+    else if(pid == 0) //if pid equal 0 - child process
+{
+            printf("This is the child process\n");
+            exit(0); // terminating the child (zombie) process
+   }
+
+    return 0;
+}
+
